47050e3fbe7507a16e6a86547339679fcffa71a0
Huy Pham <huyducphamofficial@gmail.com>
2026-03-22 19:07:36 +0700

    VC-16 | Adding the option to customize the footer description from the Admin panel

--- models/StoreSettings.js
+++ models/StoreSettings.js
@@ -13,6 +13,7 @@ const storeSettingsSchema = new mongoose.Schema(
         paymentQRCode: { type: String },
         logo: { type: String },
         favicon: { type: String },
+        footerDescription: { type: String, default: 'Experience the best coffee in town. We roast our own beans and craft each cup with care.' },
     },
     { timestamps: true }
 );
